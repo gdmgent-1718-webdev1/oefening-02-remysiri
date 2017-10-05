325dd524ccd28253d11df93d34330a27a08f50cd
remysiri <Lymih@MacBook-Pro-de-Remy.local>
2017-10-05 10:05:44 +0200

    Update

--- index.php
+++ index.php
@@ -6,12 +6,10 @@
 </head>
 <body>
 
-<? php
-	$string1 = 'HELLO';
-	$string2 = 'WORLD';
-
-echo $string1 . '' . $string2;
-
+<?php
+    $string1 = 'Hello';
+    $string2 = 'World';
+    echo $string1  . ' ' .  $string2;
 ?>
 </body>
 </html>

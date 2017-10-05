845d05271fceb74cebbd62047959009de65b3794
remysiri <Lymih@MacBook-Pro-de-Remy.local>
2017-10-05 09:53:47 +0200

    Update

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<? php
+	$string1 = 'HELLO';
+	$string2 = 'WORLD';
+
+echo $string1 . '' . $string2;
+
+?>
+</body>
+</html>
